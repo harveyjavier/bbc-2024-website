904dcb849b7aff4e99f7f7480766a5abfadae982
Harvey Javier <harveyjavier17@gmail.com>
2024-10-11 12:27:44 +0800

    Separate partners logo and date images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,32 @@
 import Image from "next/image";
 import poster from "./images/poster.png";
 import design from "./images/design.png";
+import logoDate from "./images/logo_and_date.png";
+import partners from "./images/partners.png";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen bg-cover bg-center bg-no-repeat bg-[#35006b]" style={{ backgroundImage: `url(${design.src})` }}>
-      {/* Image as background with proper scaling */}
-      <div className="absolute inset-0">
+    <div className="w-full h-screen bg-cover bg-center bg-no-repeat bg-[#35006b] grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20" style={{ backgroundImage: `url(${design.src})` }}>
+      
+      <main className="flex flex-col gap-4 row-start-2 items-center">
         <Image
-          src={poster}
-          alt="Poster"
-          layout="fill"
-          objectFit="contain" // Ensure the image fits within the container while maintaining its aspect ratio
+          src={logoDate}
+          alt="Logo and Date"
+          width={400}
+          //layout="fill"
+          //objectFit="contain" // Ensure the image fits within the container while maintaining its aspect ratio
           objectPosition="center" // Center the image in the view
           priority // Optional: load image faster
         />
-      </div>
-      {/* Your content can go here */}
+      </main>
+      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
+        <Image
+          src={partners}
+          alt="Partners"
+          width={400}
+          priority
+        />
+      </footer>
     </div>
   );
 }
